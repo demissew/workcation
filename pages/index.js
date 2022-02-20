5ed67e4f3d2946e03cb6b6f67c2758545d77bc9f
Demissew (Home) <dhex30@gmail.com>
2022-02-21 00:29:50 +0300

    Break point 1024px

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,14 @@ export default function Home() {
             <SearchFilters/>
             <main className="py-4">
 
-                <div className="px-4">
-                    <h3 className="text-gray-900 text-xl">Los Angeles</h3>
-                    <p className="text-gray-600">Live like a star ... blah blah blah</p>
+                <div className="px-4 flex justify-between items-end">
+                    <div>
+                        <h3 className="text-gray-900 text-xl">Los Angeles</h3>
+                        <p className="text-gray-600">Live like a star ... blah blah blah</p>
+                    </div>
+                    <div className="hidden sm:block">
+                        <a href="#" className="text-sm font-semibold text-indigo-600 hover:text-indigo-400">View all</a>
+                    </div>
                 </div>
 
                 <div className="sm:flex sm:gap-6 sm:overflow-x-auto sm:pb-8 px-4">
